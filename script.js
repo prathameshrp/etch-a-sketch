16120284ba2125ae7338daac54cee8ef99e93f05
prathamesh <prathamesh.math@gmail.com>
2024-07-17 03:00:39 +0530

    Add logic to make a trail of color changed div     on hover
        This ensures the basic behavior of etch-a-sketch

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ for(let i = 0; i < 16*16; ++i)
     const div = document.createElement("div");
     div.setAttribute("class", "item");
     container.appendChild(div);
-    
 }
+
+container.addEventListener("mouseover", (e)=>{
+    e.stopPropagation();
+    const inElement = e.target;
+    
+    inElement.style.backgroundColor = "blue";
+})
