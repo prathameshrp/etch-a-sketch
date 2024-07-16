2169a2094119732a21828a3e85d634a06533d89b
prathamesh <prathamesh.math@gmail.com>
2024-07-17 03:41:01 +0530

    Add a button that takes custom input
        to replace previous children with the new ones

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const container = document.querySelector(".container");
 
-let gridSize = prompt("please enter the grid size! (<100)");
 
-let boxSize = 800/gridSize;
+let boxSize = 50;
 
+function generateGrid(gridSize) {
+    container.replaceChildren();
+    for (let i = 0; i < gridSize ** 2; ++i) {
+        const div = document.createElement("div");
+        div.setAttribute("class", "item");
+        div.setAttribute("style", `height: ${boxSize}px; width: ${boxSize}px;`);
+        container.appendChild(div);
+    }
+}
+generateGrid(16);
+container.addEventListener("mouseover", (e) => {
+    e.stopPropagation();
 
-for(let i = 0; i < gridSize**2; ++i)
-{
-    const div = document.createElement("div");
-    div.setAttribute("class", "item");
-    div.setAttribute("style", `height: ${boxSize}px; width: ${boxSize}px;`);
-    container.appendChild(div);
+    const inElement = e.target;
+    if (inElement.classList.contains("item"))
+        inElement.style.backgroundColor = "blue";
+})
+
+function userInput() {
+    let gridSize = prompt("please enter the grid size! (<100)");
+    return gridSize;
 }
 
+const btn = document.querySelector("button");
 
-container.addEventListener("mouseover", (e)=>{
-    e.stopPropagation();
-    
-    const inElement = e.target;
-    if(inElement.classList.contains("item"))
-        inElement.style.backgroundColor = "blue";
+btn.addEventListener("click", () => {
+    const sz = userInput();
+    boxSize = 800 / sz;
+    generateGrid(sz);
+
 })
