b21400b93148579426dbc3172c4c299d75b019af
prathamesh <prathamesh.math@gmail.com>
2024-07-17 02:49:54 +0530

    set up structure
        Add grid for the etch-a-sketch layout

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 16*16; ++i)
+{
+    const div = document.createElement("div");
+    div.setAttribute("class", "item");
+    container.appendChild(div);
+    
+}
