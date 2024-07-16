df92ecb5c19786213209e219d458bcf5586e09a4
prathamesh <prathamesh.math@gmail.com>
2024-07-17 03:29:43 +0530

    add user input feature to generate grid
        for customizing grid options

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16*16; ++i)
+let gridSize = prompt("please enter the grid size! (<100)");
+
+let boxSize = 800/gridSize;
+
+
+for(let i = 0; i < gridSize**2; ++i)
 {
     const div = document.createElement("div");
     div.setAttribute("class", "item");
+    div.setAttribute("style", `height: ${boxSize}px; width: ${boxSize}px;`);
     container.appendChild(div);
 }
+
 
 container.addEventListener("mouseover", (e)=>{
     e.stopPropagation();
